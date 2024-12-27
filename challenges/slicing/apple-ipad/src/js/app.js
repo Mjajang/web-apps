5d2836776ca78d7f45ba76961a0b948866744db8
Jajang mahrul <jajangmahrul22@gmail.com>
2024-12-27 09:03:54 +0700

    feat: add accordion section for iPad compatibility with dynamic image updates

--- challenges/slicing/apple-ipad/src/js/app.js
+++ challenges/slicing/apple-ipad/src/js/app.js
@@ -9,3 +9,36 @@ var swiper = new Swiper(".swiper", {
     prevEl: ".btn-prev",
   },
 });
+function toggleAccordion(event) {
+  const question = event.currentTarget;
+  const content = question.nextElementSibling;
+  const svgIcon = question.querySelector("svg");
+  const imageAccordion = document.getElementById("image-accordion");
+
+  // close other toggle
+  document.querySelectorAll(".accordion .content").forEach((item) => {
+    item.style.maxHeight = null;
+    item.previousElementSibling.querySelector("svg").classList.remove("rotate-180");
+  });
+
+  // toggle current accordion
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+    svgIcon.classList.remove("rotate-180");
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+    svgIcon.classList.add("rotate-180");
+  }
+
+  switch (question.id) {
+    case "ipad-iphone":
+      imageAccordion.src = "./assets/accordion/ipad-iphone.png";
+      break;
+    case "ipad-mac":
+      imageAccordion.src = "./assets/accordion/ipad-mac.png";
+      break;
+    case "ipad-watch":
+      imageAccordion.src = "./assets/accordion/ipad-watch.jpg";
+      break;
+  }
+}
