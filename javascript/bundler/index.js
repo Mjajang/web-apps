940a43a33547313de49e9f61653157b54809f3f5
Jajang mahrul <jajangmahrul22@gmail.com>
2025-03-20 08:28:50 +0700

    feat: add initial Webpack configuration and setup for React project

---
+++ javascript/bundler/index.js
@@ -0,0 +1,34 @@
+/* WEBPACK */
+/* 
+
+*/
+
+/* TREE SHAKING DENGAN IMPORT */
+/* 
+
+*/
+
+/* KONFIGURASI WEBPACK */
+/* 
+
+*/
+
+/* BABEL AND POLYFILL */
+/* 
+
+*/
+
+/* ANALISA BUNDLE */
+/* 
+
+*/
+
+/* CREATE REACT APP DAN VITE */
+/* 
+
+*/
+
+/* INSTALL REACT USING VITE */
+/* 
+- npm create vite@latest my-vue-app -- --template react
+*/
