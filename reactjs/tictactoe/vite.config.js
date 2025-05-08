7c02c29e4ac987d2851f9fdc30c7acf4c05183a0
Jajang mahrul <jajangmahrul22@gmail.com>
2025-05-08 21:28:06 +0700

    chore: update package.json and vite.config.js for deployment configuration and new dependencies

--- reactjs/tictactoe/vite.config.js
+++ reactjs/tictactoe/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/web-apps/reactjs/tictactoe/", // Adjust to your repository's subdirectory
+});
